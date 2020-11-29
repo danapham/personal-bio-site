d7b592f42bddd73e5af32db94cf8a9def51f7727
Dana Pham <danapham12@gmail.com>
2020-11-29 15:47:02 -0600

    last touch

--- src/javascripts/components/bio.js
+++ src/javascripts/components/bio.js
@@ -10,6 +10,7 @@ const displayBio = () => {
           <p>Hi. I’m Dana - a full-stack engineer from Nashville and part-time student at Nashville Software School.</p>
           <p>My post-college life took me on a meandering path in which I took on titles such as recruiter, English teacher, world traveler, HR coordinator and now — software developer. I’ve spent the last 6 months creating 22 javascript applications and have found that my apps tend to be minimalist and user-focused, with clean, organized code. I love that development is a craft that can be continuously fine-tuned with many different areas of specialization. At the moment, I am most interested in creating apps with React, data manipulation, and animating with code.</p>
           <p>As a developer on your team, you can expect me to place strong emphasis on communication, planning, and team board games.</p>
+          <p>Want to learn more? Let's chat! Message me on <a class="p-link" href="https://www.linkedin.com/in/danapham-/">LinkedIn</a>.</p>
         </div>
       </div>
       `
