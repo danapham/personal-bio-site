ac687df42822738c0787cfc9c69d4ab4ea6a7024
Dana Pham <danapham12@gmail.com>
2021-01-23 21:41:28 -0600

    changed photo name

--- src/javascripts/components/bio.js
+++ src/javascripts/components/bio.js
@@ -6,7 +6,7 @@ const displayBio = () => {
       `
       <div class="page-container">
         <div class="bio-description">
-          <img class="profile-pic" src="src/images/IMG_3152 (1).jpg">
+          <img class="profile-pic" src="src/images/profilepic.jpg">
           <p>Hi. I’m Dana - a full-stack engineer from Nashville and part-time student at Nashville Software School.</p>
           <p>My post-college life took me on a meandering path in which I took on titles such as recruiter, English teacher, world traveler, HR coordinator and now — software developer. I’ve spent the last 6 months creating 22 javascript applications and have found that my apps tend to be minimalist and user-focused, with clean, organized code. I love that development is a craft that can be continuously fine-tuned with many different areas of specialization. At the moment, I am most interested in creating apps with React, data manipulation, and animating with code.</p>
           <p>As a developer on your team, you can expect me to place strong emphasis on communication, planning, and team board games.</p>
@@ -23,7 +23,7 @@ const initialLoad = () => {
     `
     <div class="page-container">
       <div class="bio-description">
-        <img class="profile-pic" src="src/images/IMG_3152 (1).jpg">
+        <img class="profile-pic" src="src/images/profilepic.jpg">
         <p>Hi. I’m Dana - a full-stack engineer from Nashville and part-time student at Nashville Software School.</p>
         <p>My post-college life took me on a meandering path in which I took on titles such as recruiter, English teacher, world traveler, HR coordinator and now — software developer. I’ve spent the last 6 months creating 22 javascript applications and have found that my apps tend to be minimalist and user-focused, with clean, organized code. I love that development is a craft that can be continuously fine-tuned with many different areas of specialization. At the moment, I am most interested in creating apps with React, data manipulation, and animating with code.</p>
         <p>As a developer on your team, you can expect me to place strong emphasis on communication, planning, and team board games!</p>
